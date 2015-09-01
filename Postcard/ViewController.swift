c7d42d4ee85ef15b2a3f1d7038dfc69ddf73db6f
eldoc86 <cortese.franc@gmail.com>
2015-09-01 22:57:49 +0200

    myFirst commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+
+    @IBOutlet weak var enterMessageTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden=false
+        messageLabel.textColor=UIColor.redColor()
+        messageLabel.text=enterMessageTextField.text
+        enterMessageTextField.resignFirstResponder()
+    }
 
 }
 
